7e1e2ed8246b3d104842ce8421010421d8506dd6
Sergey <elec84@mail.ru>
2022-07-02 17:39:26 +0300

    update lesson3

--- lesson3/src/main/java/TelephoneDirectory.java
+++ lesson3/src/main/java/TelephoneDirectory.java
@@ -6,14 +6,14 @@ public class TelephoneDirectory {
     String telephone;
     String surname;
     String result;
-    HashMap<String, Object> list = new HashMap<>();
-    Set<Map.Entry<String, Object>> entrySet = list.entrySet();
+    HashMap<String, String> list = new HashMap<>();
+    Set<Map.Entry<String, String>> entrySet = list.entrySet();
 
     public void add(String telephone, String surname) {
         list.put(telephone, surname);
     }
     public void get(String result) {
-        for (Map.Entry<String, Object> integerObjectEntry :entrySet){
+        for (Map.Entry<String, String> integerObjectEntry :entrySet){
             if (result.equals(integerObjectEntry.getValue())) {
                 System.out.println(integerObjectEntry.getValue() + " => " + integerObjectEntry.getKey());
             }
